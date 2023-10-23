f06905b477eedcccf7aabe3e7d15a3e0861f1a09
+ <+>
2023-10-24 00:03:04 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide (){
+    return 2000 / 100
+
+}
+console.log(divide())
+
+const square = x => x **2
+let x = 2
+console.log (square(2))
+
+const add = (x, y) => x + y
+console.log(add(4, 10))
